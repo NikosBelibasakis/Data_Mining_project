0bb6a0bc664c6ff69e690072fc8df266088da266
mira <miraslamaj@gmail.com>
2024-06-03 19:12:25 +0300

    classification evaluation last run

--- Q2/evaluation.py
+++ Q2/evaluation.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix, roc_curve, auc
 
 def get_roc_curve(model,X_test,y_test, ifNN=False):
@@ -13,12 +14,14 @@ def get_roc_curve(model,X_test,y_test, ifNN=False):
     tpr = dict()
     roc_auc = dict()
     for i in range(12):
+        if np.sum(y_test == i) == 0:
+            continue
         fpr[i], tpr[i], _ = roc_curve(y_test == i, y_score[:, i])
         roc_auc[i] = auc(fpr[i], tpr[i])
 
     # Plot ROC curve for each class
     plt.figure()
-    for i in range(12):
+    for i in fpr:
         plt.plot(fpr[i], tpr[i], lw=2,
                 label='ROC curve of class {0} (area = {1:0.2f})'
                 ''.format(i, roc_auc[i]))
@@ -35,7 +38,7 @@ def get_roc_curve(model,X_test,y_test, ifNN=False):
 def general_evaluation(y_test,y_pred):
     # Evaluate the model
     accuracy = accuracy_score(y_test, y_pred)
-    classification_rep = classification_report(y_test, y_pred)
+    classification_rep = classification_report(y_test, y_pred, zero_division=0)
 
     # Print the results
     print(f"Accuracy: {accuracy:.2f}")
